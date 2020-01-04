cfe904a34345c6bd22c2841c5268563753bb1e94
nickk-knack <nick.chiconas@gmail.com>
2020-01-04 17:01:59 -0600

    add secrets plugin

---
+++ plugins/secrets.js
@@ -0,0 +1,62 @@
+module.exports.secretMessages = {
+	// need to replace these ids with discord user ids
+	32344351: [
+		'fuck off, andrew',
+		'are you done puking yet, andrew?',
+		'andrew fucks his cat for relief',
+		'alexa, add small black anal beads to my shopping list',
+		'"casually conservative"',
+		'ah yes, thats because algebra',
+		'my cat has aids 😂',
+		'ayyy john arbuckle whats crackalackin binglebanger',
+	],
+	23245751: [
+		'nick is a bitch',
+		'high nick',
+		'danny rick lookin\' fucker',
+	],
+	37488539: [
+		'go photoshop a bigger dick on yourself, jake',
+		'.bad jake',
+		'jake go back to no lifing wow classic',
+		'shut up anprim',
+	],
+	38353365: [
+		'max, shouldn\'t you be on call? lil ho',
+		'max, you MUST contact me',
+		'want a sprite cranberry?',
+		'max, are you a believer?',
+	],
+	29852683: [
+		'matt, what\'s the derivative of e^x? bitch',
+		'who is this dude?',
+		'dumb question',
+	],
+	57184367: [
+		'the epic memer has spoken',
+		'guys i lost my mom',
+		'this guy needs more random messages',
+	],
+	43198226: [
+		'faewfwexxdb', 'babdnerionpko', 'zxvcxnkvrnoiniogarwniogsre', 'ajfewanoifjewanfewajifewjaoifewaioj', 'zxcvzxc', 'badabadbabdbadba',
+		'badabingbadaboom',
+		'O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA',
+		'you wanna hear a random signAAAaAALALAANFESNOVNSIUVBUOSEHUO#Y*R$HY*t8934try8943ry89438943wy89t43wh84e8',
+		'sSSSSS AAaAAA  R EWwE e eeE xDDDDDDDD',
+	],
+	8477170: [
+		'tik tok',
+		'chris big gay',
+	],
+};
+
+module.exports.guildPhrases = {
+	'625766841779879967': {
+		'epic': [
+			'ok now THIS is epic',
+		],
+		'never have i ever': [
+			'*puts finger down*',
+		],
+	},
+};
