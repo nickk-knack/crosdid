5841701265f05a117425235125ff226ce2a9624e
Chiconas <nick.chiconas@okstate.edu>
2020-01-06 14:18:47 -0600

    add announcements helper

--- plugins/events.js
+++ plugins/events.js
@@ -5,6 +5,7 @@ module.exports.emojiCreateChannel = {
 	// cuddling bar : emoji-town
 	'625766841779879967': {
 		channel_id: '649660798221680652',
+		channel: 'emoji-town',
 		send_emoji: true,
 		send_message: true,
 		message_prepend: true,
@@ -19,6 +20,7 @@ module.exports.emojiUpdateChannel = {
 	// cuddling bar : emoji-town
 	'625766841779879967': {
 		channel_id: '649660798221680652',
+		channel: 'emoji-town',
 		send_old_emoji: true,
 		send_new_emoji: true,
 		send_message: true,
@@ -26,6 +28,24 @@ module.exports.emojiUpdateChannel = {
 		messages: [
 			'an emoji has been updated:',
 			'new old emoji:',
+		],
+	},
+};
+
+module.exports.announcements = {
+	announcements_channel: 'announcement-town',
+	channel_create: {
+		enabled: true,
+		messages: [
+			'new channel alert:',
+			'check out the new channel:',
+		],
+	},
+	channel_delete: {
+		enabled: true,
+		messages: [
+			'rip to the following channel:',
+			'i deleted the shit out of a channel:',
 		],
 	},
 };
