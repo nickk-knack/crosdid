957fa8ee944811ff161e046b2ec773c6a9c7c777
Chiconas <nick.chiconas@okstate.edu>
2020-01-06 15:34:34 -0600

    change secretMessages to replies and add reactions

--- plugins/secrets.js
+++ plugins/secrets.js
@@ -1,55 +1,100 @@
-module.exports.secretMessages = {
-	// need to replace these ids with discord user ids
-	'250815455315492864': [
-		'fuck off, andrew',
-		'are you done puking yet, andrew?',
-		'andrew fucks his cat for relief',
-		'alexa, add small black anal beads to my shopping list',
-		'"casually conservative"',
-		'ah yes, thats because algebra',
-		'my cat has aids 😂',
-		'ayyy john arbuckle whats crackalackin binglebanger',
-	],
-	'140225600966098944': [
-		'nick is a bitch',
-		'high nick',
-		'danny rick lookin\' fucker',
-	],
-	'206037113433620480': [
-		'go photoshop a bigger dick on yourself, jake',
-		'.bad jake',
-		'jake go back to no lifing wow classic',
-		'shut up anprim',
-	],
-	38353365: [
-		'max, shouldn\'t you be on call? lil ho',
-		'max, you MUST contact me',
-		'want a sprite cranberry?',
-		'max, are you a believer?',
-	],
-	29852683: [
-		'matt, what\'s the derivative of e^x? bitch',
-		'who is this dude?',
-		'dumb question',
-	],
-	57184367: [
-		'the epic memer has spoken',
-		'guys i lost my mom',
-		'this guy needs more random messages',
-	],
-	43198226: [
-		'faewfwexxdb', 'babdnerionpko', 'zxvcxnkvrnoiniogarwniogsre', 'ajfewanoifjewanfewajifewjaoifewaioj', 'zxcvzxc', 'badabadbabdbadba',
-		'badabingbadaboom',
-		'O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA',
-		'you wanna hear a random signAAAaAALALAANFESNOVNSIUVBUOSEHUO#Y*R$HY*t8934try8943ry89438943wy89t43wh84e8',
-		'sSSSSS AAaAAA  R EWwE e eeE xDDDDDDDD',
-	],
-	8477170: [
-		'tik tok',
-		'chris big gay',
-	],
+// guild_id.user_id.messages/reactions
+module.exports.replies = {
+	'625766841779879967': {
+		// need to replace these ids with discord user ids
+		'250815455315492864': {
+			messages: [
+				'fuck off, andrew',
+				'are you done puking yet, andrew?',
+				'andrew fucks his cat for relief',
+				'alexa, add small black anal beads to my shopping list',
+				'"casually conservative"',
+				'ah yes, thats because algebra',
+				'my cat has aids 😂',
+				'ayyy john arbuckle whats crackalackin binglebanger',
+			],
+			reactions: [
+				{ custom: false, emoji: '' },
+			],
+		},
+		'140225600966098944': {
+			messages: [
+				'nick is a bitch',
+				'high nick',
+				'danny rick lookin\' fucker',
+			],
+			reactions: [
+				{ custom: false, emoji: '' },
+			],
+		},
+		'206037113433620480': {
+			messages: [
+				'go photoshop a bigger dick on yourself, jake',
+				'.bad jake',
+				'jake go back to no lifing wow classic',
+				'shut up anprim',
+			],
+			reactions: [
+				{ custom: false, emoji: '' },
+			],
+		},
+		38353365: {
+			messages: [
+				'max, shouldn\'t you be on call? lil ho',
+				'max, you MUST contact me',
+				'want a sprite cranberry?',
+				'max, are you a believer?',
+			],
+			reactions: [
+				{ custom: false, emoji: '' },
+			],
+		},
+		29852683: {
+			messages: [
+				'matt, what\'s the derivative of e^x? bitch',
+				'who is this dude?',
+				'dumb question',
+			],
+			reactions: [
+				{ custom: false, emoji: '' },
+			],
+		},
+		57184367: {
+			messages: [
+				'the epic memer has spoken',
+				'guys i lost my mom',
+				'this guy needs more random messages',
+			],
+			reactions: [
+				{ custom: false, emoji: '' },
+			],
+		},
+		43198226: {
+			messages: [
+				'faewfwexxdb', 'babdnerionpko', 'zxvcxnkvrnoiniogarwniogsre', 'ajfewanoifjewanfewajifewjaoifewaioj', 'zxcvzxc', 'badabadbabdbadba',
+				'badabingbadaboom',
+				'O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA',
+				'you wanna hear a random signAAAaAALALAANFESNOVNSIUVBUOSEHUO#Y*R$HY*t8934try8943ry89438943wy89t43wh84e8',
+				'sSSSSS AAaAAA  R EWwE e eeE xDDDDDDDD',
+			],
+			reactions: [
+				{ custom: false, emoji: '' },
+			],
+		},
+	},
+	'og server id': {
+		'182615528383184896': {
+			messages: [
+				'tik tok',
+			],
+			reactions: [
+				{ custom: true, emoji: '268177866926194690' },
+			],
+		},
+	},
 };
 
+// guild_id.phrase_activator.phrase
 module.exports.guildPhrases = {
 	'625766841779879967': {
 		'epic': [
