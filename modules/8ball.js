2f4f181a2cbf6ed0762eedb827769807310cf3bb
Nick Chiconas <nick.chiconas@gmail.com>
2020-01-06 20:58:07 -0600

    import modules from previous version

---
+++ modules/8ball.js
@@ -0,0 +1,45 @@
+const responses = [
+	'It is certain.',
+	'It is decidedly so.',
+	'Without a doubt.',
+	'Yes, definitely.',
+	'You may rely on it.',
+	'As I see it, yes.',
+	'Most likely.',
+	'The outlook is good.',
+	'Yes.',
+	'Fuck ya dude',
+	'Reply hazy, try again.',
+	'Ask again later.',
+	'Better not tell you now...',
+	'Cannot predict now.',
+	'Maybe??? Why the fuck would I know that?',
+	'Don\'t count on it.',
+	'My reply is no.',
+	'My sources say no.',
+	'Very doubtful.',
+	'Fuck no, bro.',
+	'Absolutely the fuck not.',
+	'Grilled cheese',
+];
+
+module.exports = {
+	name: '8ball',
+	aliases: ['8', '8b', 'eightball'],
+	description: 'Ask the magic 8-ball a question.',
+	args: false,
+	guildOnly: false,
+	usage: '<question>',
+	execute(message, args) {
+		if (!args.length) {
+			return message.reply('You didn\'t ask a question!');
+		}
+
+		if (args.join(' ').toLowerCase() == 'am i stupid?') {
+			message.reply('\u22c5.\u22c5');
+			return;
+		}
+
+		message.reply(responses[Math.floor(Math.random() * responses.length)]);
+	},
+};
---
+++ modules/dice.js
@@ -0,0 +1,32 @@
+module.exports = {
+	name: 'dice',
+	aliases: ['d'],
+	description: 'Rolls an n-sided die. Default: 20',
+	usage: '[sides]',
+	args: false,
+	guildOnly: false,
+	execute(message, args) {
+		let dice = 20;
+		if (args.length) {
+			const tryDice = parseInt(args[0]);
+			if (!isNaN(tryDice)) {
+				dice = tryDice;
+			}
+		}
+
+		if (dice <= 0) {
+			message.reply('you don\'t know how dice work.');
+			return;
+		}
+
+		const roll = Math.floor((Math.random() * dice) + 1);
+		let replyAppend = '';
+		if (roll == 1) {
+			replyAppend = 'Critical fail!';
+		}
+		else if (roll == dice) {
+			replyAppend = 'Critical success!';
+		}
+		message.reply(`you rolled a ${roll}. ${replyAppend}`);
+	},
+};
