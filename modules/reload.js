07748929f4f7e61a4173972eb954675586505c4f
Chiconas <nick.chiconas@okstate.edu>
2020-01-14 09:38:08 -0600

    add ability to reload commands as well

--- modules/reload.js
+++ modules/reload.js
@@ -1,15 +1,37 @@
+const fs = require('fs');
+
 module.exports = {
 	name: 'reload',
 	description: 'Forces a reload of the database.',
+	usage: '<db | commands | both>',
+	args: true,
 	guildOnly: false,
-	// todo: enable args, let user choose to reload db or commands or both
-	args: false,
 	cooldown: 5,
-	// todo: add user permissions, make this have permissionLevel:admin or smth
+	opOnly: true,
 	execute(message, args) {
-		const { db } = message.client;
+		const { db, commands } = message.client;
 
-		db.read();
+		if (args[0] === 'db' || args[0] === 'both') {
+			console.log('Reading database...');
+			db.read();
+		}
+
+		if (args[0] === 'commands' || args[0] === 'both') {
+			console.log('Reloading commands...');
+			commands.clear();
+
+			const commandModules = fs.readdirSync('./modules');
+
+			for (const file of commandModules) {
+				// If file is not a .js file or contained in the blacklist, skip it
+				if (!file.endsWith('.js')) continue;
+				if (db.get('globalDisabledCmdModules').includes(file.split('.')[0]).value()) continue;
+
+				// Require the command module and set it in the client
+				const command = require(`./modules/${file}`);
+				commands.set(command.name, command);
+			}
+		}
 
 		message.reply('reload complete!');
 	},
