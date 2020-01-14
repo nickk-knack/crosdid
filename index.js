935fb60b9668ab38518e762dac6534d902f44530
Chiconas <nick.chiconas@okstate.edu>
2020-01-14 11:14:02 -0600

    working reload now

--- index.js
+++ index.js
@@ -46,37 +46,31 @@ console.log(`Loaded local database file from ${dbFileName}`);
 
 // Extra functions
 const getRandomFromArray = array => array[Math.floor(Math.random() * array.length)];
-const reload = (reloadDB, reloadCommands) => {
-	if (reloadDB) {
-		console.log('Reading database...');
-		db.read();
-	}
-
-	if (reloadCommands) {
-		console.log('Loading command modules...');
-		client.commands.clear();
-
-		const commandModules = fs.readdirSync('./modules');
-
-		for (const file of commandModules) {
-			// If file is not a .js file or contained in the blacklist, skip it
-			if (!file.endsWith('.js')) continue;
-			if (db.get('globalDisabledCmdModules').includes(file.split('.')[0]).value()) continue;
-
-			// Require the command module and set it in the client
-			const command = require(`./modules/${file}`);
-			client.commands.set(command.name, command);
-		}
-
-		console.log(`\tCommand modules loaded. Skipped the following: ${db.get('globalDisabledCmdModules').value().join(', ')}.`);
-	}
-};
 
 // Start of the main bot code
 console.log('Starting bot...');
 
 // Load in all command modules
-reload({ reloadDB: false, reloadCommands: true });
+console.log('\tLoading command modules...');
+
+const commandModules = fs.readdirSync('./modules');
+
+for (const file of commandModules) {
+	// If file is not a .js file or contained in the blacklist, skip it
+	if (!file.endsWith('.js')) continue;
+	if (db.get('globalDisabledCmdModules').includes(file.split('.')[0]).value()) continue;
+
+	// Require the command module and set it in the client
+	try {
+		const command = require(`./modules/${file}`);
+		client.commands.set(command.name, command);
+	}
+	catch (e) {
+		console.error(`Error loading ${file}: `, e);
+	}
+}
+
+console.log(`\t\tCommand modules loaded. Skipped the following: ${db.get('globalDisabledCmdModules').value().join(', ')}.`);
 
 // Events
 console.log('\tLoading events...');
@@ -151,143 +145,131 @@ client.on('ready', () => {
 
 // Message event (command processing)
 client.on('message', msg => {
-	try {
-		// Command needs to start with prefix
-		const prefixRegex = new RegExp(`^(<@!?${client.user.id}> |\\${prefix})\\s*`);
+	// Command needs to start with prefix
+	const prefixRegex = new RegExp(`^(<@!?${client.user.id}> |\\${prefix})\\s*`);
 
-		// Test if the message was a command
-		if (!prefixRegex.test(msg.content)) {
-			// This is a regular message, do any other processing on it
+	// Test if the message was a command
+	if (!prefixRegex.test(msg.content)) {
+		// This is a regular message, do any other processing on it
 
-			// Ignore messages from bots (especially yourself), and don't process non-guild messages
-			if (msg.author.bot || typeof msg.guild === 'undefined' || msg.guild === null) return;
+		// Ignore messages from bots (especially yourself), and don't process non-guild messages
+		if (msg.author.bot || typeof msg.guild === 'undefined' || msg.guild === null) return;
 
-			// Check that the guild is configured with secret messages and reacts AND that the user has configured messages/reacts
-			const userSecrets = db.get(`${msg.guild.id}.users`).find({ id: msg.author.id }).value();
-			if (typeof userSecrets !== 'undefined') {
-				// Calculate if we want to send a message and/or react
-				const sendSecretMessage = Math.random() > (1 - process.env.SECRET_MESSAGE_CHANCE);
-				const secretlyReact = Math.random() > (1 - process.env.SECRET_REACT_CHANCE);
+		// Check that the guild is configured with secret messages and reacts AND that the user has configured messages/reacts
+		const userSecrets = db.get(`${msg.guild.id}.users`).find({ id: msg.author.id }).value();
+		if (typeof userSecrets !== 'undefined') {
+			// Calculate if we want to send a message and/or react
+			const sendSecretMessage = Math.random() > (1 - process.env.SECRET_MESSAGE_CHANCE);
+			const secretlyReact = Math.random() > (1 - process.env.SECRET_REACT_CHANCE);
 
-				// Get random message to send, send it
-				if (sendSecretMessage) {
-					const message = getRandomFromArray(userSecrets.messages);
-					msg.reply(message);
-				}
-
-				// Get random reaction, react with it
-				if (secretlyReact) {
-					const reaction = getRandomFromArray(userSecrets.reactions);
-					msg.react(reaction.custom ? msg.guild.emojis.get(reaction.emoji) : reaction.emoji);
-				}
+			// Get random message to send, send it
+			if (sendSecretMessage) {
+				const message = getRandomFromArray(userSecrets.messages);
+				msg.reply(message);
 			}
 
-			// Guild-based, phrase-activated messages
-			const enablePhrases = db.get(`${msg.guild.id}.enablePhrases`).value();
-			if (enablePhrases) {
-				const guildPhrases = db.get(`${msg.guild.id}.phrases`).value();
-				if (typeof guildPhrases !== 'undefined') {
-					for (const guildPhrase of guildPhrases) {
-						if (msg.content.includes(guildPhrase.trigger) && guildPhrase.responses) {
-							msg.channel.send(getRandomFromArray(guildPhrase.responses));
-						}
+			// Get random reaction, react with it
+			if (secretlyReact) {
+				const reaction = getRandomFromArray(userSecrets.reactions);
+				msg.react(reaction.custom ? msg.guild.emojis.get(reaction.emoji) : reaction.emoji);
+			}
+		}
+
+		// Guild-based, phrase-activated messages
+		const enablePhrases = db.get(`${msg.guild.id}.enablePhrases`).value();
+		if (enablePhrases) {
+			const guildPhrases = db.get(`${msg.guild.id}.phrases`).value();
+			if (typeof guildPhrases !== 'undefined') {
+				for (const guildPhrase of guildPhrases) {
+					if (msg.content.includes(guildPhrase.trigger) && guildPhrase.responses) {
+						msg.channel.send(getRandomFromArray(guildPhrase.responses));
 					}
 				}
 			}
+		}
 
-			// Ass-fixer
-			const assMessages = [];
-			const assTokens = msg.content.toLowerCase().match(/(\w*[\s-])ass(\s\w*)/g);
-			if (assTokens && assTokens !== null) {
-				for (const assToken of assTokens) {
-					const fixedAss = assToken.match(/ass(\s\w*)/g)[0].replace(/\s/, '-');
-					assMessages.push(fixedAss);
-				}
+		// Ass-fixer
+		const assMessages = [];
+		const assTokens = msg.content.toLowerCase().match(/(\w*[\s-])ass(\s\w*)/g);
+		if (assTokens && assTokens !== null) {
+			for (const assToken of assTokens) {
+				const fixedAss = assToken.match(/ass(\s\w*)/g)[0].replace(/\s/, '-');
+				assMessages.push(fixedAss);
 			}
-
-			if (assMessages.length > 0) msg.reply(assMessages.join(', '));
-
-			// At the end, return.
-			return;
 		}
 
-		// Get command args and command name
-		const [, matchedPrefix] = msg.content.match(prefixRegex);
-		const args = msg.content.slice(matchedPrefix.length).split(/ +/);
-		const commandName = args.shift().toLowerCase();
+		if (assMessages.length > 0) msg.reply(assMessages.join(', '));
 
-		// Get other related info
-		const isOp = db.get(`${msg.guild.id}.users`).find({ id: msg.author.id }).get('operator').value();
-		const disabledCmdModules = db.get(`${msg.guild.id}.disabledCmdModules`).value();
+		// At the end, return.
+		return;
+	}
 
-		// Special case: reload command
-		if (commandName === 'reload') {
-			reload({
-				reloadDB: args ? args[0] === 'db' || args[0] === 'both' : true,
-				reloadCommands: args ? args[0] === 'commands' || args[0] === 'cmds' || args[0] === 'both' : true,
-			});
+	// Get command args and command name
+	const [, matchedPrefix] = msg.content.match(prefixRegex);
+	const args = msg.content.slice(matchedPrefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-			return msg.reply('reload complete!');
+	// Get the actual command object, check if it exists
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	if (!command) {
+		return msg.reply('That command does not exist!');
+	}
+
+	// Check if the command is in the guild's disabledCmdModules list
+	const disabledCmdModules = db.get(`${msg.guild.id}.disabledCmdModules`).value();
+	if (disabledCmdModules && disabledCmdModules.includes(command.name)) {
+		return msg.reply('That command does not exist!');
+	}
+
+	// Check if the user can execute the command (opOnly)
+	const isOp = db.get(`${msg.guild.id}.users`).find({ id: msg.author.id }).get('operator').value();
+	if (command.opOnly && !isOp) {
+		return msg.reply('you do not have permission to execute that command!');
+	}
+
+	// Check if command needs to be sent in a server
+	if (command.guildOnly && msg.channel.type !== 'text') {
+		return msg.reply('I can\'t execute that command in a DM.');
+	}
+
+	// Check if args are required
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${msg.author}.`;
+
+		if (command.usage) {
+			reply += `\nProper usage: "${prefix}${command.name} ${command.usage}"`;
 		}
 
-		// Get the actual command object, check if it exists
-		const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-		if (!command) {
-			return msg.reply('That command does not exist!');
+		return msg.channel.send(reply);
+	}
+
+	// Set cooldown
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown) * 1000;
+
+	if (!timestamps.has(msg.author.id)) {
+		timestamps.set(msg.author.id, now);
+		setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount);
+	}
+	else {
+		const expirationTime = timestamps.get(msg.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return msg.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before using the ${command.name} command.`);
 		}
 
-		// Check if the command is in the guild's disabledCmdModules list
-		if (disabledCmdModules && disabledCmdModules.includes(command.name)) {
-			return msg.reply('That command does not exist!');
-		}
+		timestamps.set(msg.author.id, now);
+		setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount);
+	}
 
-		// Check if the user can execute the command (opOnly)
-		if (command.opOnly && !isOp) {
-			return msg.reply('you do not have permission to execute that command!');
-		}
-
-		// Check if command needs to be sent in a server
-		if (command.guildOnly && msg.channel.type !== 'text') {
-			return msg.reply('I can\'t execute that command in a DM.');
-		}
-
-		// Check if args are required
-		if (command.args && !args.length) {
-			let reply = `You didn't provide any arguments, ${msg.author}.`;
-
-			if (command.usage) {
-				reply += `\nProper usage: "${prefix}${command.name} ${command.usage}"`;
-			}
-
-			return msg.channel.send(reply);
-		}
-
-		// Set cooldown
-		if (!cooldowns.has(command.name)) {
-			cooldowns.set(command.name, new Discord.Collection());
-		}
-
-		const now = Date.now();
-		const timestamps = cooldowns.get(command.name);
-		const cooldownAmount = (command.cooldown) * 1000;
-
-		if (!timestamps.has(msg.author.id)) {
-			timestamps.set(msg.author.id, now);
-			setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount);
-		}
-		else {
-			const expirationTime = timestamps.get(msg.author.id) + cooldownAmount;
-
-			if (now < expirationTime) {
-				const timeLeft = (expirationTime - now) / 1000;
-				return msg.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before using the ${command.name} command.`);
-			}
-
-			timestamps.set(msg.author.id, now);
-			setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount);
-		}
-
-		// Execute command
+	// Execute command
+	try {
 		command.execute(msg, args);
 	}
 	catch (error) {
---
+++ modules/reload.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+
+module.exports = {
+	name: 'reload',
+	description: 'Reloads the database and/or command modules',
+	usage: '[all] [-c [command] | -d]',
+	args: true,
+	guildOnly: false,
+	opOnly: true,
+	cooldown: 3,
+	execute(message, args) {
+		const mode = args[0].toLowerCase();
+		const { db } = message.client;
+
+		if (mode === '-d' || mode === 'all') {
+			console.log('Reading database...');
+			db.read();
+		}
+
+		if (mode === '-c' || mode === 'all') {
+			if (args.length > 1) {
+				console.log('Reloading single command...');
+				const commandName = args[1].toLowerCase();
+				const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+				if (!command) {
+					return message.reply(`there is no command with name/alias \`${commandName}\`.`);
+				}
+
+				delete require.cache[require.resolve(`./${commandName}.js`)];
+
+				try {
+					const newCommand = require(`./${commandName}.js`);
+					message.client.commands.set(newCommand.name, newCommand);
+				}
+				catch (e) {
+					console.error(e);
+					return message.reply('there was an error while reloading that command.');
+				}
+			}
+			else {
+				console.log('Reloading all commands');
+				const commandModules = fs.readdirSync('./modules');
+
+				for (const file of commandModules) {
+					if (!file.endsWith('.js')) continue;
+					if (db.get('globalDisabledCmdModules').includes(file.split('.')[0]).value()) continue;
+
+					delete require.cache[require.resolve(`./${file}`)];
+
+					try {
+						const command = require(`./${file}`);
+						message.client.commands.set(command.name, command);
+					}
+					catch (e) {
+						console.error(e);
+						return message.reply(`there was an error while reloading commands at ${file}`);
+					}
+				}
+
+				console.log(`\tAll command modules reloaded. Skipped the following: ${db.get('globalDisabledCmdModules').value().join(', ')}.`);
+			}
+		}
+
+		message.reply('reload complete!');
+	},
+};
