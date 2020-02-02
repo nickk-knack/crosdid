15c5ca3dc91bad177b4158edf26bf71a26577022
Nick Chiconas <nick.chiconas@gmail.com>
2020-02-01 21:25:53 -0600

    convert tabs to two spaces, also add ping time

--- modules/test.js
+++ modules/test.js
@@ -1,11 +1,11 @@
 module.exports = {
-	name: 'test',
-	description: 'Test command for testing purposes',
-	args: false,
-	guildOnly: false,
-	opOnly: true,
-	cooldown: 1,
-	execute(message, args) {
-		message.channel.send('Yeah, that\'s how commands work.');
-	},
+  name: 'test',
+  description: 'Test command for testing purposes',
+  args: false,
+  guildOnly: false,
+  opOnly: true,
+  cooldown: 1,
+  execute(message, args) {
+    message.channel.send(`Yeah, that's how commands work. Ping time: ${Date.now() - message.createdTimestamp}ms`);
+  },
 };
